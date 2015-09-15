df66e016192606abdcf8b5afa52679acbe047f9c
Garrett Sand <garrettsand@gmail.com>
2015-09-15 14:55:39 -0700

    Finished Chapter 4

--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class CanAccessHomeTest < Capybara::Rails::TestCase
   test "sanity" do
     visit root_path
-    assert_content page, "Hello World"
+    assert_content page, "Garrett Sand"
     refute_content page, "Goobye All!"
   end
 end
